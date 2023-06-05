fc42e268770b781bdacf4cfdb920ae47a7cd430f
yaxley-peaks <epiclycoolgaemer@gmail.com>
2023-06-05 17:38:25 +0530

    Moved to regex based parsing

--- src/line_parser.rs
+++ src/line_parser.rs
@@ -1,16 +1,24 @@
+use regex::Regex;
+
 #[derive(Debug)]
 pub enum Kind {
     Num(u32),
     Range((u32, u32)),
 }
 
-fn parse_value(val: &str) -> Result<Kind, String> {
+fn parse_value(val: &str) -> Kind {
     let parse_try = val.parse::<u32>();
+    let re = Regex::new(r"(?P<init>\d+)-(?P<fin>\d+)").unwrap();
     // Move to regex based parsing
     match parse_try {
-        Ok(v) => Ok(Kind::Num(v)),
+        Ok(v) => Kind::Num(v),
         Err(_) => {
-            todo!()
+            let caps = re.captures(val).expect("Match failed");
+            return Kind::Range((caps["init"].parse().unwrap(), caps["fin"].parse().unwrap()));
         }
     }
+}
+
+pub fn parse_line_ranges(vals: Vec<String>) -> Vec<Kind> {
+    vals.iter().map(|x| parse_value(x)).collect()
 }
