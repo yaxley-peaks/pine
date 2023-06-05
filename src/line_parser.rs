9da59340a9fa5207ff1c1da199a3054021ac28fe
yaxley-peaks <epiclycoolgaemer@gmail.com>
2023-06-05 22:06:06 +0530

    Unit testing

--- src/line_parser.rs
+++ src/line_parser.rs
@@ -3,10 +3,19 @@ use std::process::exit;
 use lazy_static::lazy_static;
 use regex::Regex;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum LineKind {
     Num(u32),
     Range((u32, u32)),
+}
+
+impl LineKind {
+    pub fn contains(&self, other: u32) -> bool {
+        match self {
+            LineKind::Num(x) => *x == other,
+            LineKind::Range((x, y)) => (*x..*y).contains(&other),
+        }
+    }
 }
 
 fn parse_value(val: &str) -> LineKind {
@@ -14,7 +23,6 @@ fn parse_value(val: &str) -> LineKind {
     lazy_static! {
         static ref RE: Regex = Regex::new(r"(?P<init>\d+)-(?P<fin>\d+)").unwrap();
     }
-    // Move to regex based parsing
     match parse_try {
         Ok(v) => LineKind::Num(v),
         Err(_) => {
@@ -30,4 +38,19 @@ fn parse_value(val: &str) -> LineKind {
 
 pub fn parse_line_ranges(vals: Vec<String>) -> Vec<LineKind> {
     vals.iter().map(|x| parse_value(x)).collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_value_with_number() {
+        assert_eq!(parse_value("123"), LineKind::Num(123));
+    }
+
+    #[test]
+    fn test_parse_value_with_range() {
+        assert_eq!(parse_value("10-20"), LineKind::Range((10, 20)));
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,11 @@ use clap::Parser;
 use line_parser::parse_line_ranges;
 use pine::{read_file_into_lines, Args};
 pub mod line_parser;
+pub mod process;
 
 fn main() {
     let args = Args::parse();
     let _x = parse_line_ranges(args.lines);
-    let y = read_file_into_lines(args.i_file);
-    println!("{y:#?}");
+    let _y = read_file_into_lines(args.i_file);
+    println!("{:#?}", _x[0].contains(3));
 }
