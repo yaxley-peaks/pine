debf1f97804febd30b682eb74ebb17bc3132238f
yaxley-peaks <epiclycoolgaemer@gmail.com>
2023-06-05 18:34:39 +0530

    Rename Kind to LineKind

--- src/line_parser.rs
+++ src/line_parser.rs
@@ -4,30 +4,30 @@ use lazy_static::lazy_static;
 use regex::Regex;
 
 #[derive(Debug)]
-pub enum Kind {
+pub enum LineKind {
     Num(u32),
     Range((u32, u32)),
 }
 
-fn parse_value(val: &str) -> Kind {
+fn parse_value(val: &str) -> LineKind {
     let parse_try = val.parse::<u32>();
     lazy_static! {
         static ref RE: Regex = Regex::new(r"(?P<init>\d+)-(?P<fin>\d+)").unwrap();
     }
     // Move to regex based parsing
     match parse_try {
-        Ok(v) => Kind::Num(v),
+        Ok(v) => LineKind::Num(v),
         Err(_) => {
             if !RE.is_match(val) {
                 eprintln!("The line format is incorrectly specified: {}", val);
                 exit(1);
             }
             let caps = RE.captures(val).expect("Match failed");
-            Kind::Range((caps["init"].parse().unwrap(), caps["fin"].parse().unwrap()))
+            LineKind::Range((caps["init"].parse().unwrap(), caps["fin"].parse().unwrap()))
         }
     }
 }
 
-pub fn parse_line_ranges(vals: Vec<String>) -> Vec<Kind> {
+pub fn parse_line_ranges(vals: Vec<String>) -> Vec<LineKind> {
     vals.iter().map(|x| parse_value(x)).collect()
 }
