f260ab41a6410e0b48666b98deedef38e3ecbac5
yaxley-peaks <epiclycoolgaemer@gmail.com>
2023-06-05 16:45:33 +0530

    Command line args

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,10 @@
+use clap::Parser;
+#[derive(Parser, Debug)]
 pub struct Args {
+    #[arg(short)]
     i_file: String,
+    #[arg(short)]
     lines: Vec<String>,
+    #[arg(short)]
     o_file: String,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use clap::Parser;
+use pine::Args;
 fn main() {
+    let args = Args::parse();
+    println!("{args:?}");
     println!("Hello, world!");
 }
