71451ac49596dd41e6c914b7db3e19f4781448a1
yaxley-peaks <epiclycoolgaemer@gmail.com>
2023-06-06 15:55:43 +0530

    Basic drawing

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,38 @@
-use clap::Parser;
-use line_parser::parse_line_ranges;
-use pine::{read_file_into_lines, Args};
-
-use crate::process::{build_final_result, mark_lines};
 pub mod line_parser;
 pub mod process;
 
-fn main() {
-    let args = Args::parse();
-    let _x = parse_line_ranges(args.lines);
-    let _y = read_file_into_lines(args.i_file);
-    let _z = read_file_into_lines(args.o_file);
-    let step1 = mark_lines(_x, _y);
-    let step2 = build_final_result(step1, _z);
-    let step2 = step2.join("\n");
-    println!("{step2}");
+use crossterm::{
+    event::{DisableMouseCapture, EnableMouseCapture},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use std::{io, thread, time::Duration};
+use tui::{
+    backend::CrosstermBackend,
+    widgets::{Block, Borders},
+    Terminal,
+};
+
+fn main() -> Result<(), io::Error> {
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    // ===============================
+
+    // draw calls
+
+    // ===============================
+
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
+    Ok(())
 }
